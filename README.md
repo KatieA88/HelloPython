d11e8ea5b9006e3636f0e4fd9d3089765b5b6c8f
Katie Adams <katieadams@Katies-MacBook-Pro.local>
2018-05-08 20:58:08 +0100

    Adding HelloWorld Python File

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HelloPython
 Hello World in Python 3
+
+This is my first python project
---
+++ helloworld_start.py
@@ -0,0 +1,9 @@
+#
+# Example file for HelloWorld
+# 
+
+Def main() :
+    print ("hello world")
+
+if __name__ == "__main__":
+    main()
